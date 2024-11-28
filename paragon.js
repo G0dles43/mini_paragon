06fecae8bf4871130c5217a401ec4e6993e928ad
G0dles43 <85917@student.pb.edu.pl>
2024-11-28 23:57:57 +0100

    express

--- paragon.js
+++ paragon.js
@@ -97,7 +97,7 @@ function printBill() {
     const currentDay = (date.getDate()<10) ? "0"+date.getDate() : date.getDate(); 
     const currentMonth = (date.getMonth()<10) ? "0"+(date.getMonth()+1) : date.getMonth()+1;
     const currentYear = date.getFullYear();
-    document.querySelector("h1").innerHTML=`Paragon ${currentDay}|${currentMonth}|${currentYear} `;
+    document.getElementById("H1").innerHTML=`Paragon ${currentDay}|${currentMonth}|${currentYear} `;
 
     localStorage.setItem("bill", JSON.stringify(bill));
 }
